b16b8bc4b11f4a7a1946e127e859ef51333ca17a
JHessler <jhessler11@gmail.com>
2022-06-04 12:11:07 -0400

    update server and mutations in client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,13 +27,40 @@ export const ADD_USER = qgl`
 export const SAVE_BOOK = qgl`
     mutation saveBook($bookId: String!, $authors: [String], $description: String!, $title: String, $image: String, $link: String) {
         saveBook(bookId: $bookId, authors: $authors, description: $description, title: $title, image: $image, link: $link)
+    {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
     }
+}
 `;
 
 export const REMOVE_BOOK = qgl`
     mutation removeBook($BookId: String!) {
         removeBook(bookId: $bookId) {
-            
+            {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+
         }
     }
 `
