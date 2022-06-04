a6763d62dbe7e1e6454bf2aba224045a5643c7f7
JHessler <jhessler11@gmail.com>
2022-06-04 10:16:35 -0400

    savebook

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,5 +13,43 @@ const resolvers = {
             }
             throw new AuthenticationError('Not logged in')
         }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+            // all conditional are pass create token
+            const token = auth.signToken(user);
+      
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = auth.signToken(user);
+      
+            return { token, user };
+        },
+
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $addToSet: { savedBooks: args }},
+                    { new: true, 
+                    runValidators: true }
+                );
+                return user
+            }
+        }
     }
 }
